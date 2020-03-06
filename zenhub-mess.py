20da36179c77b96130ece7218e4166a55cd6e19b
Lukewh <luke@lukewh.com>
2020-03-06 09:22:47 +0000

    flask app

--- zenhub-mess.py
+++ zenhub-mess.py
@@ -2,6 +2,8 @@ import requests
 import json
 from dotenv import load_dotenv
 
+import flask
+
 load_dotenv()
 import os
 
@@ -44,9 +46,7 @@ headers = {
     "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
 }
 
-r = requests.get(URL, headers=headers)
-
-issues = r.json()
+app = flask.Flask(__name__)
 
 
 def get_epics(issues):
@@ -89,9 +89,6 @@ def issues_as_dict(issues):
     return issues_dict
 
 
-issues_dict = issues_as_dict(issues)
-
-
 def add_to_parents(issues, issues_dict):
     for issue in issues:
         parents = issue["parent_epics"]
@@ -108,19 +105,78 @@ def add_to_parents(issues, issues_dict):
     return issues_dict
 
 
-issues_dict = add_to_parents(issues, issues_dict)
+def update_estimates(issues_dict):
+    for issue in issues_dict:
+        single_issue = issues_dict[issue]
 
-issues_list = []
+        if not single_issue["estimate"]:
+            single_issue["estimate"] = 0
 
-for issue in issues_dict.items():
-    if issue[1]["is_epic"] or len(issue[1]["children"]) > 0:
-        issues_list.append(issue[1])
+        if not "complete" in single_issue:
+            single_issue["complete"] = 0
 
-print(
-    json.dumps(
-        filter_by_milestone(
-            issues_list, "Iteration 20-08 (17 February - 28 February)"
-        ),
-        indent=2,
-    )
-)
+        if single_issue["children"]:
+            for child in single_issue["children"]:
+                if child["estimate"]:
+                    single_issue["estimate"] += child["estimate"]
+                    if child["state"] == "closed":
+                        single_issue["complete"] += child["estimate"]
+
+
+def simpler_issues(issues):
+    new_issues = []
+
+    for issue in issues:
+        is_master_epic = False
+
+        for label in issue["labels"]:
+            if "name" in label and label["name"] == "Master Epic":
+                is_master_epic = True
+
+        new_issues.append(
+            {
+                "estimate": issue["estimate"],
+                "html_url": issue["html_url"],
+                "labels": issue["labels"],
+                "milestone": issue["milestone"],
+                "issue_number": issue["number"],
+                "organization_name": issue["organization_name"],
+                "parent_epics": issue["parent_epics"],
+                "releases": issue["releases"],
+                "repo_id": issue["repo_id"],
+                "repo_name": issue["repo_name"],
+                "state": issue["state"],
+                "is_epic": issue["is_epic"],
+                "title": issue["title"],
+                "is_master_epic": is_master_epic,
+            }
+        )
+
+    return new_issues
+
+
+@app.route("/")
+def get_the_data_big_boy():
+    milestone = flask.request.args.get("milestone")
+
+    r = requests.get(URL, headers=headers)
+
+    issues = simpler_issues(r.json())
+
+    issues_dict = add_to_parents(issues, issues_as_dict(issues))
+
+    update_estimates(issues_dict)
+
+    issues_list = []
+
+    for issue in issues_dict.items():
+        if issue[1]["is_epic"] or len(issue[1]["children"]) > 0:
+            issues_list.append(issue[1])
+
+    if milestone:
+        return flask.jsonify(filter_by_milestone(issues_list, milestone))
+    else:
+        return flask.jsonify(issues_list)
+
+
+app.run()
